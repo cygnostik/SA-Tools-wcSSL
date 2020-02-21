98dc2bc55677283c49277f490cea40bb8c285e94
cygnostik <chris.mish@gmail.com>
2020-02-20 17:19:39 -0800

    more typos

--- wildcard.sh
+++ wildcard.sh
@@ -3,7 +3,7 @@ echo "Run with your email address and domain name as options."
 echo "example: ./wildcard.sh you@company.com company.com"
 echo "\n"
 echo "If you cannot create a TXT record in your zone NOW press CTRL-C to abort"
-read -n 1 -s -r -p "You are $1 and generating wildcard SSL for $2? (press any key or CTRL-C to abort)"
+read -s -r -p "You are $1 and generating wildcard SSL for $2? (press any key or CTRL-C to abort)"
 clear
 certbot certonly --manual --preferred-challenges=dns --email $1 --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d *.$2
 echo "\n"
@@ -12,7 +12,7 @@ echo "They are already located in /etc/letsencrypt/live/$2/"
 echo "\n"
 read -p "Press [Enter] for ESXi use or CTRL-C to exit."
 clear
-echo "Copying your new wildcard SSL files to: $2-`date +"%F"`"
+echo "Copying your new wildcard SSL files to: $2-`date +r'%Fr'`"
 mkdir $2-`date +"%F"`
 cp /etc/letsencrypt/live/$2/fullchain.pem ./$2-`date +"%F"`/castore.pem
 cp /etc/letsencrypt/live/$2/cert.pem ./$2-`date +"%F"`/rui.cert
