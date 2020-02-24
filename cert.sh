0d8747bfcd48840a3425bd905f6d317ebe52ca8f
cygnostik <chris.mish@gmail.com>
2020-02-23 18:19:10 -0800

    Little more tuning

--- cert.sh
+++ cert.sh
@@ -2,5 +2,5 @@
 echo "You'll need to be ready to add a DNS record to continue..."
 echo "\n"
 echo "Run with cert.sh email@addie.com SSL-Domain.FQDN or *.domain.com"
-read -p "Press CRTL+C to abord, [Enter] to continue."
+read -s -r -p "Press CRTL+C to abord, [Enter] to continue."
 certbot certonly --manual --preferred-challenges=dns --email $1 --server https://acme-v02.api.letsencrypt.org/directory --agree-tos -d $2
--- wildcard.sh
+++ wildcard.sh
@@ -10,7 +10,7 @@ echo "\n"
 echo "The certbot script has now run, hopefully you're all set with details on your SSL files above."
 echo "They are already located in /etc/letsencrypt/live/$2/"
 echo "\n"
-read -p "Press [Enter] for ESXi use or CTRL-C to exit."
+read -s -r -p "Press any key for ESXi extra steps or CTRL-C to exit."
 clear
 echo "Copying your new wildcard SSL files to: $2-`date +r'%Fr'`"
 mkdir $2-`date +"%F"`
